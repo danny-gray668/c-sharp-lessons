bd75f9beb1f58e4f22edc0b407966357497a9fbd
Danny Gray <renodtg@gmail.com>
2026-09-17 13:36:00 -0600

    Initial Commit of Local C# Training Material

---
+++ tutorials/NumbersTutorial/Program.cs
@@ -0,0 +1,76 @@
+﻿//=======================================================
+//              Chapter 1: Hello World 
+//=======================================================
+// HelloWorld();
+
+// void HelloWorld()
+// {
+//     ////////////// Section 1 //////////////////////
+//     // Basics
+//     Console.WriteLine("Hello World!!");
+
+//     string aFriend = "Jack";
+//     Console.WriteLine("Hello " + aFriend);
+//     aFriend = "Sophia";
+//     Console.WriteLine("Hello " + aFriend);
+//     Console.WriteLine($"Hello {aFriend}");
+
+//     ////////////// Section 2 //////////////////////
+//     // Calling Variables
+//     string firstFriend = "Joe";
+//     string secondFriend = "Brad";
+
+//     Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+//     Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+
+//     ////////////// Section 3 //////////////////////
+ 
+//     // Trimming 
+//      string greeting = "      Hello World!       ";
+//     Console.WriteLine($"[{greeting}]");
+
+//     string trimmedGreeting = "";
+//     trimmedGreeting = greeting.TrimStart();
+//     Console.WriteLine($"[{trimmedGreeting}]");
+//     trimmedGreeting = greeting.TrimEnd();
+//     Console.WriteLine($"[{trimmedGreeting}]");
+//     trimmedGreeting = greeting.Trim();
+//     Console.WriteLine($"[{trimmedGreeting}]");
+
+//     // Simple Text Formatting
+//     Console.WriteLine(greeting.ToUpper());
+//     Console.WriteLine(greeting.ToLower());
+
+//     // Replacing
+//     string sayHello = "Hello World!";
+//     Console.WriteLine(sayHello);
+//     sayHello = sayHello.Replace("Hello", "Greetings");
+//     Console.WriteLine(sayHello);
+
+
+//     // Contains
+//     // Console.Clear();
+//     string songLyrics = "You say goodbye, and I say hello";
+//     Console.WriteLine(songLyrics.Contains("goodbye"));
+//     Console.WriteLine(songLyrics.Contains("greetings"));
+// }
+
+//=======================================================
+//              Section 2: Numbers 
+//=======================================================
+
+
+////////////// Section 1 //////////////////////
+WorkWithIntegers();
+
+void WorkWithIntegers()
+{
+    // Definitions
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+    c = a + b - 12 * 17;
+    Console.WriteLine(c);
+}
---
+++ tutorials/numbers.cs
@@ -0,0 +1,5 @@
+
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
